0395e158597ce8ce5b9fd1ca67d251b710855e53
Vince489 <vincentcornelius489@gmail.com>
2026-02-02 10:01:06 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// The "Hello World" route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
