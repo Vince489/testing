41b738cfd49326e0c47593406f299d138839f807
Vince489 <vincentcornelius489@gmail.com>
2026-02-02 10:08:53 -0500

    index.js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// The "Hello World" route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Serve static files from the current directory
+app.use(express.static('.'));
 
 // Start the server
 app.listen(PORT, () => {
